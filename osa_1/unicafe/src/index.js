2203d954512088874d8506f39d333c2b8687d83a
kkemppi <kemppi.mikko@gmail.com>
2020-06-03 14:18:34 +0300

    1.7 oikeasti tehty

--- osa_1/unicafe/src/index.js
+++ osa_1/unicafe/src/index.js
@@ -11,13 +11,17 @@ const App = () => {
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>good</button>
-      <button onClick={() => setBad(bad + 1)}>good</button>
+        <button onClick={() => setGood(good + 1)}>good</button>
+        <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+        <button onClick={() => setBad(bad + 1)}>bad</button>
       <h1>statistics</h1>
-      good {good} <br/>
-      neutral {neutral} <br/>
-      bad {bad}
+        <p>
+          good {good} <br/>
+          neutral {neutral} <br/>
+          bad {bad} <br/>
+          average {(good - bad) / (good + bad + neutral)} <br/>
+          positive {(good / (good + neutral + bad)) * 100} %
+        </p>
     </div>
   )
 }
