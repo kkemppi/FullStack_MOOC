b1dac9199d4e7eee9c22de4d609104a2493f0171
kkemppi <kemppi.mikko@gmail.com>
2020-06-03 15:49:05 +0300

    1.10 tehty

--- osa_1/unicafe/src/index.js
+++ osa_1/unicafe/src/index.js
@@ -12,16 +12,28 @@ const Statistics = ({good, neutral, bad}) => {
   }
   return (
     <div>
-      <p>
-        good {good} <br/>
-        neutral {neutral} <br/>
-        bad {bad} <br/>
-        average {(good - bad) / sum} <br/>
-        positive {(good / sum) * 100} %
-      </p>
+        <StatisticLine text = "good" value = {good}/>
+        <StatisticLine text = "neutral" value = {neutral}/>
+        <StatisticLine text = "bad" value = {bad}/>
+        <StatisticLine text = "average" value = {(good - bad) / sum}/>
+        <StatisticLine text = "positive" value = {(good / sum) * 100 + " %"}/>
     </div>
   )
 }
+
+const Button = ({onClick, text}) => {
+  return (
+    <button onClick={onClick}>{text}</button>
+  )
+}
+
+const StatisticLine = ({text, value, mark}) => {
+  return (
+   <div>{text} {value} <br/></div>
+  )
+}
+
+
 
 const App = () => {
   // tallenna napit omaan tilaansa
@@ -33,9 +45,9 @@ const App = () => {
   return (
     <div>
       <h1>give feedback</h1>
-        <button onClick={() => setGood(good + 1)}>good</button>
-        <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-        <button onClick={() => setBad(bad + 1)}>bad</button>
+        <Button onClick={() => setGood(good + 1)} text = "good"/>
+        <Button onClick={() => setNeutral(neutral + 1)} text = "neutral"/>
+        <Button onClick={() => setBad(bad + 1)} text = "bad"/>
       <h1>statistics</h1>
         <Statistics good = {good} neutral = {neutral} bad = {bad}/>
     </div>
