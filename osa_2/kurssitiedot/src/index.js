07c0e64d504beefa56ebb4a197f909ae3e7aaf9c
kkemppi <kemppi.mikko@gmail.com>
2020-06-09 11:33:16 +0300

    2.3 asti tehty

---
+++ osa_2/kurssitiedot/src/index.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = (props) => <h1>{props.course}</h1>
+
+const Part = ({name, exercise}) => <li>{name} {exercise}</li>
+
+
+const Content = ({parts}) => {
+  return (
+    <div>
+      {parts.map(parts => <Part key= {parts.id} name = {parts.name} exercise = {parts.exercises}/>)}
+    </div>
+  )
+}
+
+
+
+const Course = ({course}) => {
+  return (
+  <div>
+    <Header course={course.name}/>
+  <ul>
+    <Content parts = {course.parts}/>
+  </ul>
+    <Total parts = {course.parts}/>
+  </div>
+  )
+}
+
+const Total = ({parts}) => {
+  const values = parts.map(parts => parts.exercises)
+  const sum = values.reduce((a, v) => a + v)
+  return (
+    <p>
+      Total of {sum} exercises
+    </p>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    id: 1,
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+  
+  return (
+    <div>
+        <Course course = {course}/>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
